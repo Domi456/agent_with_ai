2373f4e8acaffe10a886d8d967f29b0e88701406
Domi456 <121254412+Domi456@users.noreply.github.com>
2025-04-04 16:25:22 +0200

    added inqurer package

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 #from vector import retriever
+import inquirer
 
 model = OllamaLLM(model="Gemma3:1b")
 
@@ -13,14 +14,26 @@ Question: {question}
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
-context_multiline = input("Enter context: ")
+questions = [
+  inquirer.List('size',
+                message="What size do you need?",
+                choices=['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+            ),
+]
+answers = inquirer.prompt(questions)
+print(answers)
+
+context_multiline = input("Enter context (or 'bye' to quit): ")
 context = " ".join(context_multiline.splitlines())
-print("\n-----------------------------------------")
 
 while True:
-    print("\n-----------------------------------------")
+    if context.lower() == 'bye':
+        print("Goodbye!")
+        break
+
+    print("-----------------------------------------")
     question = input("Enter your question (or 'bye' to quit): ")
-    if question.lower() == 'bye' or context.lower() == 'bye':
+    if question.lower() == 'bye':
         print("Goodbye!")
         break
 
